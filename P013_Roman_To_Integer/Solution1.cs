86261d208b46d8a6f0cfca4e193d0d284b0f5ca1
adrian_sun <ziqingyi@qq.com>
2020-10-19 23:05:57 +1100

    roman to int

---
+++ P013_Roman_To_Integer/Solution1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P013_Roman_To_Integer
+{
+    public static class Solution1
+    {
+        public static int RomanToInt(string s)
+        {
+
+            if (string.IsNullOrEmpty(s)) return 0;
+            var dic = new Dictionary<char, int> {
+                {'I', 1}, {'V', 5},{'X', 10},{'L', 50},{'C', 100},{'D', 500},{'M', 1000}
+            };
+            int res = 0;
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                var curVal = dic[s[i]];
+                var nextVal = dic[s[i + 1]];
+                if (curVal < nextVal) res -= curVal;
+                else res += curVal;
+            }
+            res += dic[s.Last()];
+            return res;
+
+
+
+
+
+
+        }
+
+    }
+}
