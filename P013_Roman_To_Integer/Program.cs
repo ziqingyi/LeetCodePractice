459d5f5391eaf730e7436d0a59f96bfc81f94c88
adrian_sun <ziqingyi@qq.com>
2020-10-25 13:49:04 +1100

    review roman to int

--- P013_Roman_To_Integer/Program.cs
+++ P013_Roman_To_Integer/Program.cs
@@ -32,24 +32,23 @@ namespace P013_Roman_To_Integer
                         Given a roman numeral, convert it to an integer.
              
                         Example 1:
-
                         Input: s = "III"
                         Output: 3
-                        Example 2:
 
+                        Example 2:
                         Input: s = "IV"
                         Output: 4
-                        Example 3:
 
+                        Example 3:
                         Input: s = "IX"
                         Output: 9
-                        Example 4:
 
+                        Example 4:
                         Input: s = "LVIII"
                         Output: 58
                         Explanation: L = 50, V= 5, III = 3.
-                        Example 5:
 
+                        Example 5:
                         Input: s = "MCMXCIV"
                         Output: 1994
                         Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
@@ -63,11 +62,9 @@ namespace P013_Roman_To_Integer
              */
             #endregion
 
+            string s1 = "MCMXCIV";
 
-
-
-
-
+            int result1 = Solution1.RomanToInt(s1);
 
 
 
--- P013_Roman_To_Integer/Solution1.cs
+++ P013_Roman_To_Integer/Solution1.cs
@@ -11,18 +11,31 @@ namespace P013_Roman_To_Integer
         public static int RomanToInt(string s)
         {
 
-            if (string.IsNullOrEmpty(s)) return 0;
+            if (string.IsNullOrEmpty(s)) 
+                return 0;
+
+
             var dic = new Dictionary<char, int> {
                 {'I', 1}, {'V', 5},{'X', 10},{'L', 50},{'C', 100},{'D', 500},{'M', 1000}
             };
+
             int res = 0;
+
             for (int i = 0; i < s.Length - 1; i++)
             {
-                var curVal = dic[s[i]];
-                var nextVal = dic[s[i + 1]];
-                if (curVal < nextVal) res -= curVal;
-                else res += curVal;
+                int curVal = dic[s[i]];
+                int nextVal = dic[s[i + 1]];
+
+                if ( curVal > nextVal )
+                {
+                    res += curVal;
+                }
+                else
+                {
+                    res -= curVal;
+                }
             }
+
             res += dic[s.Last()];
             return res;
 
