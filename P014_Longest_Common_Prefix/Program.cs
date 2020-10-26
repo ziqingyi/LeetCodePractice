53e70cb4135beffdd7e1d4b96c5e54fb3af2c175
adrian_sun <ziqingyi@qq.com>
2020-10-26 22:42:03 +1100

    longest common prefix

---
+++ P014_Longest_Common_Prefix/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P014_Longest_Common_Prefix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Write a function to find the longest common prefix string amongst an array of strings.
+
+            If there is no common prefix, return an empty string "".
+
+             
+
+            Example 1:
+
+            Input: strs = ["flower","flow","flight"]
+            Output: "fl"
+            Example 2:
+
+            Input: strs = ["dog","racecar","car"]
+            Output: ""
+            Explanation: There is no common prefix among the input strings.
+             
+
+            Constraints:
+
+            0 <= strs.length <= 200
+            0 <= strs[i].length <= 200
+            strs[i] consists of only lower-case English letters.
+
+             */
+
+
+            string[] strs1 = new String[]{"flower", "flow", "flight"} ;
+
+            string result1 = Solution1.LongestCommonPrefix(strs1);
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ P014_Longest_Common_Prefix/Solution1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P014_Longest_Common_Prefix
+{
+    public static class Solution1
+    {
+
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            if (strs == null || strs.Length <= 0)
+            {
+                return "";
+            }
+
+            string prefix = "";
+
+            for (int j = 0; j < strs[0].Length; j++)
+            {
+                char c = (strs[0])[j];
+
+                for (int i = 1; i < strs.Length; i++)
+                {
+                    if (j >= strs[i].Length || (strs[i])[j] != c)
+                    {
+                        return prefix;
+                    }
+                }
+
+                prefix += c;
+
+
+
+            }
+
+
+            return prefix;
+
+        }
+
+    }
+}
