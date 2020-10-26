53e85ab03d022a7fd4d1178393c17675e1189337
adrian_sun <ziqingyi@qq.com>
2020-10-26 23:01:48 +1100

    valid parentheses

---
+++ P020_Valid_Parentheses/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P020_Valid_Parentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+            An input string is valid if:
+
+            Open brackets must be closed by the same type of brackets.
+            Open brackets must be closed in the correct order.
+             
+
+            Example 1:
+            Input: s = "()"
+            Output: true
+
+            Example 2:
+
+            Input: s = "()[]{}"
+            Output: true
+
+            Example 3:
+
+            Input: s = "(]"
+            Output: false
+
+            Example 4:
+
+            Input: s = "([)]"
+            Output: false
+
+            Example 5:
+
+            Input: s = "{[]}"
+            Output: true
+             
+
+            Constraints:
+
+            1 <= s.length <= 104
+            s consists of parentheses only '()[]{}'.
+
+    
+             */
+
+
+
+
+
+
+        }
+    }
+}
---
+++ P020_Valid_Parentheses/Solution1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P020_Valid_Parentheses
+{
+    public static class Solution1
+    {
+        public bool IsValid(string s)
+        {
+
+
+
+        }
+
+    }
+}
