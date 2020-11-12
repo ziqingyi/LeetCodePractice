532220dbf7d552c86040c5daaadeadcd2bb40b47
adrian_sun <ziqingyi@qq.com>
2020-11-12 22:52:47 +1100

    merge two sorted list

---
+++ P021_Merge_Two_Sorted_Lists/Solution1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P021_Merge_Two_Sorted_Lists
+{
+
+    //Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution1
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+
+
+
+        }
+
+
+
+    }
+}
