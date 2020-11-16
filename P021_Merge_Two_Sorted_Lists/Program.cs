f7671026e4158daffb4ba1fc2f30227da85a9c20
adrian_sun <ziqingyi@qq.com>
2020-11-16 22:55:12 +1100

    point in solution 1 for merge two sorted list

--- P021_Merge_Two_Sorted_Lists/Program.cs
+++ P021_Merge_Two_Sorted_Lists/Program.cs
@@ -32,9 +32,23 @@ namespace P021_Merge_Two_Sorted_Lists
                 The number of nodes in both lists is in the range [0, 50].
                 -100 <= Node.val <= 100
                 Both l1 and l2 are sorted in non-decreasing order.
-             
-             
              */
+
+            ListNode a1 = new ListNode(1,null);
+            ListNode a2 = new ListNode(2,null);
+            ListNode a3 = new ListNode(4, null);
+            a1.next = a2;
+            a2.next = a3;
+
+            ListNode b1 = new ListNode(1, null);
+            ListNode b2 = new ListNode(3, null);
+            ListNode b3 = new ListNode(4, null);
+            b1.next = b2;
+            b2.next = b3;
+
+            ListNode result1 = Solution1.MergeTwoLists(a1, b1);
+
+
 
 
         }
--- P021_Merge_Two_Sorted_Lists/Solution1.cs
+++ P021_Merge_Two_Sorted_Lists/Solution1.cs
@@ -21,11 +21,31 @@ namespace P021_Merge_Two_Sorted_Lists
 
     public class Solution1
     {
-        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        public static ListNode MergeTwoLists(ListNode l1, ListNode l2)
         {
+            if (l1 == null)
+            {
+                return l2;
+            }
+            if (l2 == null)
+            {
+                return l1;
+            }
 
+            ListNode mergedList = null;
 
+            if (l1.val < l2.val)
+            {
+                mergedList = l1;
+                mergedList.next = MergeTwoLists(l1.next, l2);
+            }
+            else
+            {
+                mergedList = l2;
+                mergedList.next = MergeTwoLists(l1, l2.next);
+            }
 
+            return mergedList;
         }
 
 
