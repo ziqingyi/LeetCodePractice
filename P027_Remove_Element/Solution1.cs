fab9928edcc3d17ce02547f9aae9195ecbc53f87
adrian_sun <ziqingyi@qq.com>
2020-11-24 22:01:35 +1100

    P027_Remove_Element

---
+++ P027_Remove_Element/Solution1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P027_Remove_Element
+{
+    public static class Solution1
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+
+
+
+
+
+        }
+
+    }
+}
