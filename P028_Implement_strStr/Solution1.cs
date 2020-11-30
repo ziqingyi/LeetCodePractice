89b3ab82b73c0e3b0ab7db44a191bbf3567c821b
adrian_sun <ziqingyi@qq.com>
2020-11-30 22:36:44 +1100

    implement strstr

---
+++ P028_Implement_strStr/Solution1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P028_Implement_strStr
+{
+    public static class Solution1
+    {
+        public static int StrStr(string haystack, string needle)
+        {
+
+
+
+        }
+    }
+}
