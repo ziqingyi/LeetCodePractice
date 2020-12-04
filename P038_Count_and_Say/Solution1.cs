145ea23b8ab1d1edb865c51f3f8ffe2ad48e9f49
adrian_sun <ziqingyi@qq.com>
2020-12-04 23:10:17 +1100

    p38 count and say

---
+++ P038_Count_and_Say/Solution1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P038_Count_and_Say
+{
+    public class Solution1
+    {
+        public static string CountAndSay(int n)
+        {
+
+
+
+        }
+
+    }
+}
