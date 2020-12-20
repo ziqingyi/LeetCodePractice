0a8bbb5811857ee9680105800cbc6681aa829395
adrian_sun <ziqingyi@qq.com>
2020-12-20 19:13:57 +1100

    P066_Plus_One solution1

---
+++ P066_Plus_One/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P066_Plus_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Given a non-empty array of decimal digits representing a non-negative integer, increment one to the integer.
+             
+            The digits are stored such that the most significant digit is at the head of the list, 
+            and each element in the array contains a single digit.
+            
+            You may assume the integer does not contain any leading zero, except the number 0 itself.
+             */
+
+            int[] input1 = {9,9,9}; //{1, 2, 3};
+            int[] output = Solution1.PlusOne(input1);
+
+
+
+
+        }
+    }
+}
---
+++ P066_Plus_One/Solution1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P066_Plus_One
+{
+    public static class Solution1
+    {
+        public static int[] PlusOne(int[] digits)
+        {
+            int total = digits.Length;
+            for (int i = total - 1; i >= 0; i--)
+            {
+                if (digits[i] == 9)
+                {
+                    digits[i] = 0;
+                }
+                else
+                {
+                    digits[i]++;
+                    return digits;
+                }
+            }
+            digits = new int[total+1];
+            digits[0] = 1;
+            return digits;
+
+        }
+    }
+}
