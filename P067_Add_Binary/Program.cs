4f78a1d4e4db31c4dd51eedb9daf5b11455ab112
adrian_sun <ziqingyi@qq.com>
2020-12-21 22:28:00 +1100

    P067_Add_Binary

---
+++ P067_Add_Binary/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P067_Add_Binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *Given two binary strings a and b, return their sum as a binary string.
+             * Example 1:
+
+                Input: a = "11", b = "1"
+                Output: "100"
+                Example 2:
+
+                Input: a = "1010", b = "1011"
+                Output: "10101"
+                 
+                Constraints:
+
+                1 <= a.length, b.length <= 104
+                a and b consist only of '0' or '1' characters.
+                Each string does not contain leading zeros except for the zero itself.
+
+             */
+            string a1= "11";
+            string b1 = "1";
+            string s1 = Solution1.AddBinary(a1, b1);
+
+
+
+        }
+    }
+}
---
+++ P067_Add_Binary/Solution1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P067_Add_Binary
+{
+    public class Solution1
+    {
+        public static string AddBinary(string a, string b)
+        {
+            //int a1 = int.Parse(a);
+            //int b1 = int.Parse(b);
+            //string r = Convert.ToString(int.Parse(a), 2)+ Convert.ToString(int.Parse(b), 2);
+
+
+            return r;
+        }
+
+
+    }
+}
