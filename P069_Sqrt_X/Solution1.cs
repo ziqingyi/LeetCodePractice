3007b45fc43e4111ed21b60f93db9aef9d6aa9c4
adrian_sun <ziqingyi@qq.com>
2021-01-09 23:08:28 +1100

    P069_Sqrt_X

---
+++ P069_Sqrt_X/Solution1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P069_Sqrt_X
+{
+    public static class Solution1
+    {
+        public static int MySqrt(int x)
+        {
+
+
+
+        }
+
+
+    }
+}
