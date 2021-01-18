46e3bf8f77bcd17892834aebec5dc8f9361d86fd
adrian_sun <ziqingyi@qq.com>
2021-01-18 22:59:10 +1100

    P069_Sqrt_X sol 1

--- P069_Sqrt_X/Solution1.cs
+++ P069_Sqrt_X/Solution1.cs
@@ -10,9 +10,14 @@ namespace P069_Sqrt_X
     {
         public static int MySqrt(int x)
         {
+            if (x < 2) return x;
 
+            int left = (int)Math.Pow(Math.E, 0.5 * Math.Log(x));
+            int right = left + 1;
 
+            int result = right * right > x ? left : right;
 
+            return result;
         }
 
 
