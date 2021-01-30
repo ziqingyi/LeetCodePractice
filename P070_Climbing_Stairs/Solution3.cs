5f4984ec06420625e3d696d2bd65db9ca0cd6f13
adrian_sun <ziqingyi@qq.com>
2021-01-30 22:56:19 +1100

    P070_Climbing_Stairs solution 3

---
+++ P070_Climbing_Stairs/Solution3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P070_Climbing_Stairs
+{
+    public static class Solution3
+    {
+        /*
+         *Runtime: 40 ms, faster than 68.55% of C# online submissions for Climbing Stairs.
+         *
+         * Memory Usage: 15.2 MB, less than 35.31% of C# online submissions for Climbing Stairs.
+         */
+        public static int ClimbStairs(int n)
+        {
+            if (n <= 2)
+            {
+                return n;
+            }
+
+            int[] res = new int[n+1];
+
+            res[1] = 1;
+            res[2] = 2;
+
+            for (int i = 3; i <= n ; i++)
+            {
+                res[i] = res[i - 1] + res[i - 2];
+            }
+
+            return res[n];
+
+        }
+
+    }
+}
