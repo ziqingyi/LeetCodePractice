561743720c937b6baac9f21061032f2a4fdec555
adrian_sun <ziqingyi@qq.com>
2021-01-31 12:06:09 +1100

    P083_Remove_Duplicates_from_Sorted_List

---
+++ P083_Remove_Duplicates_from_Sorted_List/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P083_Remove_Duplicates_from_Sorted_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *Given the head of a sorted linked list,
+             *delete all duplicates such that each element appears only once.
+             *Return the linked list sorted as well.
+             */
+
+
+
+
+
+        }
+    }
+
+
+    //Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ P083_Remove_Duplicates_from_Sorted_List/Solution1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P083_Remove_Duplicates_from_Sorted_List
+{
+    public static class Solution1
+    {
+        public static ListNode DeleteDuplicates(ListNode head)
+        {
+
+
+
+
+
+        }
+
+
+    }
+}
