99054b54206c562202ff9cce526227a4424cf92d
adrian_sun <ziqingyi@qq.com>
2021-01-31 12:49:24 +1100

    P083_Remove_Duplicates_from_Sorted_List solution 1

--- P083_Remove_Duplicates_from_Sorted_List/Program.cs
+++ P083_Remove_Duplicates_from_Sorted_List/Program.cs
@@ -14,9 +14,19 @@ namespace P083_Remove_Duplicates_from_Sorted_List
              *Given the head of a sorted linked list,
              *delete all duplicates such that each element appears only once.
              *Return the linked list sorted as well.
+             *
+             * The number of nodes in the list is in the range [0, 300].
+             * -100 <= Node.val <= 100
+             * The list is guaranteed to be sorted in ascending order.
              */
 
+            ListNode n5 = new ListNode(3, null);
+            ListNode n4 = new ListNode(3, n5);
+            ListNode n3 = new ListNode(2,n4);
+            ListNode n2 = new ListNode(1,n3);
+            ListNode head = new ListNode(1,n2);
 
+            ListNode newHead = Solution1.DeleteDuplicates(head);
 
 
 
--- P083_Remove_Duplicates_from_Sorted_List/Solution1.cs
+++ P083_Remove_Duplicates_from_Sorted_List/Solution1.cs
@@ -8,13 +8,26 @@ namespace P083_Remove_Duplicates_from_Sorted_List
 {
     public static class Solution1
     {
+        /*
+         *Runtime: 96 ms, faster than 67.46% of C# online submissions .
+         * Memory Usage: 26.7 MB, less than 13.29% of C# online submissions.
+         *
+         */
         public static ListNode DeleteDuplicates(ListNode head)
         {
-
-
-
-
-
+            ListNode current = head;
+            while (current != null && current.next != null)
+            {
+                if (current.next.val == current.val)
+                {
+                    current.next = current.next.next;
+                }
+                else
+                {
+                    current = current.next;
+                }
+            }
+            return head;
         }
 
 
