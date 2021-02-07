238990d76908c881ec43dbb275c2543e56d8a7e2
adrian_sun <ziqingyi@qq.com>
2021-02-07 13:16:06 +1100

    P101_Symmetric_Tree solution 1

---
+++ P101_Symmetric_Tree/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P101_Symmetric_Tree
+{
+    /** Definition for a binary tree node.*/
+   public class TreeNode {
+       public int val;
+       public TreeNode left;
+       public TreeNode right;
+       public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+   }
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Given a binary tree, check whether it is a mirror of itself(ie, symmetric around its center).
+
+            For example, this binary tree[1, 2, 2, 3, 4, 4, 3] is symmetric:
+            But the following [1,2,2,null,3,null,3] is not:
+            Follow up: Solve it both recursively and iteratively.
+            */
+
+            TreeNode tree1 = new TreeNode(1, 
+                new TreeNode(2, null, new TreeNode(3)), 
+                new TreeNode(2, null, new TreeNode(3)));
+            
+            bool isSame1 = Solution1.IsSymmetric(tree1);
+
+
+
+            TreeNode tree2 = new TreeNode(1, 
+               new TreeNode(2, new TreeNode(3), new TreeNode(4)), 
+               new TreeNode(2, new TreeNode(4), new TreeNode(3)));
+
+            bool isSame2 = Solution1.IsSymmetric(tree2);
+
+
+        }
+    }
+}
---
+++ P101_Symmetric_Tree/Solution1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P101_Symmetric_Tree
+{
+    public static class Solution1
+    {
+        /**
+         *Runtime: 100 ms, faster than 38.78% of C# online submissions for Symmetric Tree.
+         *Memory Usage: 26 MB, less than 12.84% of C# online submissions for Symmetric Tree.
+         */
+
+
+        public static bool IsSymmetric(TreeNode root)
+        {
+            if (root is null)
+            {
+                return true;
+            }
+
+            return isMirror(root.left, root.right);
+
+        }
+
+        public static bool isMirror(TreeNode left, TreeNode right)
+        {
+            if (left == null || right == null)
+            {
+                return left == right;//check whether all null
+            }
+
+            if (left.val != right.val)
+            {
+                return false;
+            }
+
+            //use for debug
+            bool isMirrorLeft = isMirror(left.left, right.right);
+            bool isMirrorRight = isMirror(left.right, right.left);
+            return isMirrorLeft && isMirrorRight;
+
+        }
+
+
+    }
+}
