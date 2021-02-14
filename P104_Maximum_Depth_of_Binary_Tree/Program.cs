41ca29f88bc87d1d2a8968543fb51b21b4d9f837
adrian_sun <ziqingyi@qq.com>
2021-02-14 22:17:01 +1100

    P104_Maximum_Depth_of_Binary_Tree

---
+++ P104_Maximum_Depth_of_Binary_Tree/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P104_Maximum_Depth_of_Binary_Tree
+{
+    //Definition for a binary tree node.
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *Given the root of a binary tree, return its maximum depth.
+             * A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+                Example 1:
+                Input: root = [3,9,20,null,null,15,7]
+                Output: 3
+
+                Example 2:
+                Input: root = [1,null,2]
+                Output: 2
+
+                Example 3:
+                Input: root = []
+                Output: 0
+
+                Example 4:
+                Input: root = [0]
+                Output: 1
+                 
+                Constraints:
+                The number of nodes in the tree is in the range [0, 104].
+                -100 <= Node.val <= 100
+             *
+             *
+             */
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ P104_Maximum_Depth_of_Binary_Tree/Solution1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P104_Maximum_Depth_of_Binary_Tree
+{
+
+
+    public static class Solution1
+    {
+        public static int MaxDepth(TreeNode root)
+        {
+
+        }
+
+
+    }
+}
