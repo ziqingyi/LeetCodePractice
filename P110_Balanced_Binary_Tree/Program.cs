b88387bd6749b450f5bebd73a9ddbc65ab995a62
adrian_sun <ziqingyi@qq.com>
2021-03-28 22:58:36 +1100

    P110_Balanced_Binary_Tree

---
+++ P110_Balanced_Binary_Tree/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P110_Balanced_Binary_Tree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *Given a binary tree, determine if it is height-balanced.
+             *
+             * For this problem, a height-balanced binary tree is defined as:
+             *
+             * a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+                
+            Input: root = [3,9,20,null,null,15,7]
+            Output: true
+            
+            Input: root = [1,2,2,3,3,null,null,4,4]
+            Output: false
+                            
+            
+            Input: root = []
+            Output: true
+                 
+                
+            Constraints:
+                
+                The number of nodes in the tree is in the range [0, 5000].
+                -104 <= Node.val <= 104
+             *
+             */
+
+
+
+
+
+        }
+    }
+}
---
+++ P110_Balanced_Binary_Tree/Solution1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P110_Balanced_Binary_Tree
+{
+    public class Solution1
+    {
+        public bool IsBalanced(TreeNode root)
+        {
+
+            return true;
+        }
+    }
+}
