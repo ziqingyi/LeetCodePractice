4f2657fc046383312762f01caee6b3bb651628a1
adrian_sun <ziqingyi@qq.com>
2021-04-19 22:48:48 +1000

    P111_Minimum_Depth_of_Binary_Tree

---
+++ P111_Minimum_Depth_of_Binary_Tree/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace P111_Minimum_Depth_of_Binary_Tree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TreeNode root1 = new TreeNode(3,
+                new TreeNode(9),
+                new TreeNode(20,
+                    new TreeNode(15),
+                    new TreeNode(7)));
+
+            bool result1 = Solution1.MinDepth(root1);
+
+        }
+    }
+}
---
+++ P111_Minimum_Depth_of_Binary_Tree/Solution1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P111_Minimum_Depth_of_Binary_Tree
+{
+    public static class Solution1
+    {
+
+        public static bool MinDepth(TreeNode root)
+        {
+
+
+
+        }
+
+
+    }
+}
