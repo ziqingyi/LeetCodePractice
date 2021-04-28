7837a0ba2d6de378a84783d8a0c10e704674f809
adrian_sun <ziqingyi@qq.com>
2021-04-28 22:03:40 +1000

    P111_Minimum_Depth_of_Binary_Tree solution1

--- P111_Minimum_Depth_of_Binary_Tree/Solution1.cs
+++ P111_Minimum_Depth_of_Binary_Tree/Solution1.cs
@@ -14,12 +14,45 @@ namespace P111_Minimum_Depth_of_Binary_Tree
                 return 0;
             }
 
+            Queue<TreeNode> q = new Queue<TreeNode>();
+
+            q.Enqueue(root);
+
+            int depth = 0;
+
+            while (q.Count != 0)
+            {
+                int numberOfNode = q.Count;
+
+                while (numberOfNode >0)
+                {
+                    TreeNode n = q.Dequeue();
+                    if (n.left == null && n.right == null)
+                    {
+                        depth++;
+                        return depth;
+                    }
+
+                    if (n.left != null)
+                    {
+                        q.Enqueue(n.left);
+                    }
+
+                    if (n.right != null)
+                    {
+                        q.Enqueue(n.right);
+                    }
+
+                    numberOfNode--;
+
+                }
+
+                depth++;
+            }
 
 
 
-
-
-
+            return depth;
 
         }
 
