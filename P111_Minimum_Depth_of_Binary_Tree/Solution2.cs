63bcb6806c5b31aab21294b3d3c03cdaa4279699
adrian_sun <ziqingyi@qq.com>
2021-05-01 23:50:03 +1000

    P111_Minimum_Depth_of_Binary_Tree sol 2

---
+++ P111_Minimum_Depth_of_Binary_Tree/Solution2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P111_Minimum_Depth_of_Binary_Tree
+{
+    public static class Solution2
+    {
+
+        public static int MinDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            if(root.left == null)
+            {
+                return MinDepth(root.right)+1;
+            }
+            if (root.right == null)
+            {
+                return MinDepth(root.left) +1;
+            }
+
+            int depthL = MinDepth(root.left);
+            int depthR = MinDepth(root.right);
+
+            int depth = depthL < depthR ? depthL +1: depthR+1;
+
+            return depth;
+
+        }
+
+
+    }
+}
