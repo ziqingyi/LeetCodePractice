1dc12f172c391925e1f3c3089a821dc3ef1368e3
adrian_sun <ziqingyi@qq.com>
2021-05-02 11:33:22 +1000

    P111_Minimum_Depth_of_Binary_Tree solution 2 review

--- P111_Minimum_Depth_of_Binary_Tree/Program.cs
+++ P111_Minimum_Depth_of_Binary_Tree/Program.cs
@@ -25,7 +25,19 @@ namespace P111_Minimum_Depth_of_Binary_Tree
                     new TreeNode(15),
                     new TreeNode(7)));
 
-            int result1 = Solution1.MinDepth(root1);
+            //int result1 = Solution1.MinDepth(root1);
+            //int result2 = Solution2.MinDepth(root1);
+
+
+            TreeNode root2 = new TreeNode(1,
+                new TreeNode(2,new TreeNode(4),
+                    new TreeNode(5)),
+                new TreeNode(3, new TreeNode(6)
+                    ));
+
+            int result3 = Solution2.MinDepth(root2);
+
+
 
         }
     }
--- P111_Minimum_Depth_of_Binary_Tree/Solution2.cs
+++ P111_Minimum_Depth_of_Binary_Tree/Solution2.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Text;
 
 namespace P111_Minimum_Depth_of_Binary_Tree
@@ -9,28 +10,45 @@ namespace P111_Minimum_Depth_of_Binary_Tree
 
         public static int MinDepth(TreeNode root)
         {
+            #region 1 If root is empty
             if (root == null)
             {
                 return 0;
             }
-            if(root.left == null)
+              
+            #endregion
+
+            int depthL;
+            int depthR;
+
+            #region 2 if one of the subtree is empty
+            //the depth of the tree is decided by the height of subtree
+            //if one side is empty, the other side's depth + 1 is the whole tree's depth.
+            if (root.left == null)
             {
-                return MinDepth(root.right)+1;
+                depthR = MinDepth(root.right) +1;
+                return depthR;
             }
+
             if (root.right == null)
             {
-                return MinDepth(root.left) +1;
+                depthL = MinDepth(root.left) +1;
+                return depthL;
             }
+            #endregion
 
-            int depthL = MinDepth(root.left);
-            int depthR = MinDepth(root.right);
+            #region 3 if both subtree is not empty, compare
 
-            int depth = depthL < depthR ? depthL +1: depthR+1;
+            depthL = MinDepth(root.left);
+            depthR = MinDepth(root.right);
 
-            return depth;
+            int depth = Math.Min(depthL, depthR) + 1;
+            return depth;            
+
+            #endregion
+
 
         }
-
-
+        
     }
 }
