3b0e29a096b544877461f2bf0f9a0ffe23d55262
adrian_sun <ziqingyi@qq.com>
2021-05-30 11:55:59 +1000

    p112 solution 1

---
+++ P112_Path_Sum/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace P112_Path_Sum
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *Given the root of a binary tree and an integer targetSum,
+             *return true if the tree has a root-to-leaf path such that adding up all the values along the path equals target Sum.
+             * A leaf is a node with no children.
+             *
+             *
+             */
+
+            TreeNode root1 = new TreeNode(5,
+                new TreeNode(4,
+                    new TreeNode(11,
+                        new TreeNode(7, 
+                            new TreeNode(2)))),
+                
+                new TreeNode(8,
+                    new TreeNode(13),
+                    new TreeNode(4, 
+                        null,
+                        new TreeNode(1)
+                    )));
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ P112_Path_Sum/Solution1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P112_Path_Sum
+{
+    public class Solution1
+    {
+
+        public static bool HasPathSum(TreeNode root, int targetSum)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+
+            if (root.val == targetSum && root.left == null && root.right == null)
+            {
+                return true;
+            }
+
+            bool result = HasPathSum(root.left, targetSum - root.val)
+                || HasPathSum(root.right, targetSum - root.val);
+
+            return result;
+        }
+
+
+    }
+}
