f47a44c8717882aaa2a9fc2948cdd736f351dde0
adrian_sun <ziqingyi@qq.com>
2021-05-30 22:30:57 +1000

    P113_Path_Sum_II, medium, work out!

--- P113_Path_Sum_II/Program.cs
+++ P113_Path_Sum_II/Program.cs
@@ -1,29 +1,69 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace P113_Path_Sum_II
 {
+
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
     class Program
     {
+        /*
+         * Given the root of a binary tree and an integer targetSum,
+         * return all root-to-leaf paths where each path's sum equals targetSum.
+         * A leaf is a node with no children.
+         */
 
-
-        public class TreeNode
-        {
-            public int val;
-            public TreeNode left;
-            public TreeNode right;
-            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-            {
-                this.val = val;
-                this.left = left;
-                this.right = right;
-            }
-        }
+        /*First Medium problem and solved by myself. 
+         *
+         * Success
+         * Details
+         *
+         * Runtime: 252 ms, faster than 36.64% of C# online submissions for Path Sum II.
+         *
+         * Memory Usage: 34.5 MB, less than 30.15% of C# online submissions for Path Sum II.
+         *
+         *
+         *
+         */
 
 
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            TreeNode root1 = new TreeNode(5,
+                new TreeNode(4,
+                    new TreeNode(11,
+                        new TreeNode(7),
+                        new TreeNode(2))),
+
+                new TreeNode(8,
+                    new TreeNode(13),
+                    new TreeNode(4,
+                        new TreeNode(5),
+                        new TreeNode(1)
+                    )));
+
+
+            IList<IList<int>> result = Solution1.PathSum(root1, 22);
+
+
+
+            Console.ReadKey();
         }
     }
 }
--- P113_Path_Sum_II/Solution1.cs
+++ P113_Path_Sum_II/Solution1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 
 namespace P113_Path_Sum_II
@@ -7,7 +9,36 @@ namespace P113_Path_Sum_II
     public class Solution1
     {
 
+        public static IList<IList<int>> PathSum(TreeNode root, int targetSum)
+        {
 
+            IList<IList<int>> paths = new List<IList<int>>();
+            
+            find_path(root, targetSum,"", paths);
+
+            return paths;
+
+        }
+
+        public static void find_path(TreeNode root,int targetSum, string path, IList<IList<int>> paths)
+        {
+            if (root != null)
+            {
+                path +=root.val.ToString(); 
+                
+                //reach a leaf and leaf route value is equal to target sum
+                if ( targetSum == root.val &&  root.left == null && root.right == null)
+                {
+                    paths.Add(path.Split(',').Select(Int32.Parse).ToList());
+                }
+                else
+                {
+                    path += ",";//extend the current path
+                    find_path(root.left,targetSum - root.val, path, paths);
+                    find_path(root.right, targetSum - root.val, path, paths);
+                }
+            }
+        }
 
 
     }
