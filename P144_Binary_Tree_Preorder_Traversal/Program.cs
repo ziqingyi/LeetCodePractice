75a770dc235a19d4c5f707acad9413db8bab4657
adrian_sun <ziqingyi@qq.com>
2021-06-05 22:33:27 +1000

    P144_Binary_Tree_Preorder_Traversal

---
+++ P144_Binary_Tree_Preorder_Traversal/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P144_Binary_Tree_Preorder_Traversal
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    class Program
+    {
+
+        //Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+        static void Main(string[] args)
+        {
+        
+            TreeNode root1 = new TreeNode(1,
+            null,
+            new TreeNode(2,
+                new TreeNode(3),
+                null));
+
+
+            IList<int> result1 = Solution1.PreorderTraversal(root1);
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ P144_Binary_Tree_Preorder_Traversal/Solution1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P144_Binary_Tree_Preorder_Traversal
+{
+    public class Solution1
+    {
+        public static IList<int> PreorderTraversal(TreeNode root)
+        {
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
