e23a95216e4311a65dad9d70bb6358dbf1019e74
adrian_sun <ziqingyi@qq.com>
2021-06-08 22:16:13 +1000

    pre order traversal of tree, dfs

--- P144_Binary_Tree_Preorder_Traversal/Solution1.cs
+++ P144_Binary_Tree_Preorder_Traversal/Solution1.cs
@@ -8,17 +8,28 @@ namespace P144_Binary_Tree_Preorder_Traversal
     {
         public static IList<int> PreorderTraversal(TreeNode root)
         {
+            IList<int> list = new List<int>();
 
+            Preorder(list,root);
 
-
-
-
-
-
-
-
+            return list;
 
         }
+
+
+        public static void Preorder(IList<int> l, TreeNode root)
+        {
+            if (root != null)
+            {
+                l.Add(root.val);
+
+                Preorder(l, root.left);
+
+                Preorder(l, root.right);
+            }
+        }
+
+
 
     }
 }
