b5ae5572bb17083cec925893a9f2f9eb428621b0
adrian_sun <ziqingyi@qq.com>
2021-06-09 20:55:16 +1000

    P226_Invert_Binary_Tree

---
+++ P226_Invert_Binary_Tree/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace P226_Invert_Binary_Tree
+{
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    class Program
+    {
+
+       
+
+        static void Main(string[] args)
+        {
+            /*
+             * Input: root = [4,2,7,1,3,6,9]
+             * Output: [4,7,2,9,6,3,1]
+             */
+
+
+
+            TreeNode root1 = new TreeNode(4,
+                new TreeNode(2, 
+                    new TreeNode(1), 
+                    new TreeNode(3)),
+                new TreeNode(7,
+                    new TreeNode(6),
+                    new TreeNode(9))
+                );
+
+
+            TreeNode result1 = Solution1.InvertTree(root1);
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ P226_Invert_Binary_Tree/Solution1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P226_Invert_Binary_Tree
+{
+    public class Solution1
+    {
+        public static TreeNode InvertTree(TreeNode root)
+        {
+
+
+
+        }
+
+
+
+
+
+    }
+}
