4e536666d824e9add2aa5d35e1781a3fcdaf33f7
adrian_sun <ziqingyi@qq.com>
2021-06-09 20:49:24 +1000

    P145_Binary_Tree_Postorder_Traversal and solution 1

---
+++ P145_Binary_Tree_Postorder_Traversal/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P145_Binary_Tree_Postorder_Traversal
+{
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            TreeNode root1 = new TreeNode(1,
+                null,
+                new TreeNode(2,
+                    new TreeNode(3),
+                    null));
+
+
+            IList<int> result1 = Solution1.PostorderTraversal(root1);
+
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ P145_Binary_Tree_Postorder_Traversal/Solution1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P145_Binary_Tree_Postorder_Traversal
+{
+    public  class Solution1
+    {
+        public static IList<int> PostorderTraversal(TreeNode root)
+        {
+            IList<int> list = new List<int>();
+
+            Postorder(list, root);
+
+            return list;
+
+        }
+
+        public static void Postorder(IList<int> l, TreeNode root)
+        {
+            if (root != null)
+            {
+                Postorder(l,root.left);
+
+                Postorder(l,root.right);
+
+                l.Add(root.val);
+            }
+
+
+        }
+
+
+    }
+}
