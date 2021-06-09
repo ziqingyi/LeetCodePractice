d5afdccda135626568b20ab5d8db9abe8e69a023
adrian_sun <ziqingyi@qq.com>
2021-06-09 20:58:14 +1000

    notes

--- P226_Invert_Binary_Tree/Program.cs
+++ P226_Invert_Binary_Tree/Program.cs
@@ -23,8 +23,20 @@ namespace P226_Invert_Binary_Tree
         static void Main(string[] args)
         {
             /*
-             * Input: root = [4,2,7,1,3,6,9]
-             * Output: [4,7,2,9,6,3,1]
+                Input: root = [4,2,7,1,3,6,9]
+                Output: [4,7,2,9,6,3,1]
+             
+                Input: root = [2,1,3]
+                Output: [2,3,1]
+                Example 3:
+
+                Input: root = []
+                Output: []
+                
+                Constraints:
+
+                The number of nodes in the tree is in the range [0, 100].
+                -100 <= Node.val <= 100
              */
 
 
