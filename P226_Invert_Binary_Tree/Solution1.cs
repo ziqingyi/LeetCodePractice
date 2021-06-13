cb9b421302d74992ae2f3a331bfd269a7f671f9d
adrian_sun <ziqingyi@qq.com>
2021-06-13 10:55:11 +1000

    P226_Invert_Binary_Tree solution 1

--- P226_Invert_Binary_Tree/Solution1.cs
+++ P226_Invert_Binary_Tree/Solution1.cs
@@ -6,16 +6,24 @@ namespace P226_Invert_Binary_Tree
 {
     public class Solution1
     {
+ 
         public static TreeNode InvertTree(TreeNode root)
         {
+            if (root == null)
+            {
+                return null;
+            }
 
+            TreeNode right = InvertTree(root.right);
 
+            TreeNode left = InvertTree(root.left);
+
+            root.left = right;
+            root.right = left;
+
+            return root;
 
         }
-
-
-
-
 
     }
 }
