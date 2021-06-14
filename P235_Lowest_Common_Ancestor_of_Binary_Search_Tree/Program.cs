c6f88859df9faaa53efe6e5f5e7d77b9da86f0a1
adrian_sun <ziqingyi@qq.com>
2021-06-14 22:07:35 +1000

    P235_Lowest_Common_Ancestor_of_Binary_Search_Tree

---
+++ P235_Lowest_Common_Ancestor_of_Binary_Search_Tree/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace P235_Lowest_Common_Ancestor_of_Binary_Search_Tree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode root1 = new TreeNode(6,
+                new TreeNode(2,
+                    new TreeNode(0),
+                    new TreeNode(4,
+                        new TreeNode(3),
+                        new TreeNode(5))),
+                new TreeNode(8,
+                    new TreeNode(7),
+                    new TreeNode(9))
+            );
+
+
+            TreeNode result1 = Solution1.LowestCommonAncestor(root1,
+                new TreeNode(2),
+                new TreeNode(8));
+
+
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ P235_Lowest_Common_Ancestor_of_Binary_Search_Tree/Solution1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P235_Lowest_Common_Ancestor_of_Binary_Search_Tree
+{
+    public class Solution1
+    {
+        public static TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+
+
+        }
+
+
+    }
+}
