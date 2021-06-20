fda33b811c6b1c1d091e0bcb36e58a14eda2837e
adrian_sun <ziqingyi@qq.com>
2021-06-20 12:47:41 +1000

    P404_Sum_of_Left_Leaves solution 1

---
+++ P404_Sum_of_Left_Leaves/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P404_Sum_of_Left_Leaves
+{
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode root2 = new TreeNode(1,
+                new TreeNode(2,
+                    new TreeNode(4),
+                    new TreeNode(5)),
+                new TreeNode(3));
+
+            int result2 = Solution1.SumOfLeftLeaves(root2);
+
+
+
+
+            TreeNode root = new TreeNode(3,
+                new TreeNode(9),
+                new TreeNode(20,
+                    new TreeNode(15),
+                    new TreeNode(7)));
+
+            int result1 = Solution1.SumOfLeftLeaves(root);
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ P404_Sum_of_Left_Leaves/Solution1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace P404_Sum_of_Left_Leaves
+{
+    public static class Solution1
+    {
+        public static int SumOfLeftLeaves(TreeNode root)
+        {
+            int result = 0;
+            GetSum(root, ref result);
+
+            return result;
+
+        }
+
+        public static void GetSum(TreeNode root,ref int sum)
+        {
+            if (root != null)
+            {
+                //must be leaves,no left node or right node. 
+                if (root.left != null && root.left.left == null && root.left.right == null)
+                {
+                    sum += root.left.val;
+                }
+
+                GetSum(root.left, ref sum);
+
+                GetSum(root.right, ref sum);
+
+            }
+
+
+        }
+
+
+    }
+}
